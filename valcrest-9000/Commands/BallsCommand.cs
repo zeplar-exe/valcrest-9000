679b72bce6141bcd97581edbed64477a57cf679b
Zander Baker <zanderbaker100@gmail.com>
2023-07-03 17:21:20 -0700

    fishfucker

--- valcrest-9000/Commands/BallsCommand.cs
+++ valcrest-9000/Commands/BallsCommand.cs
@@ -12,4 +12,12 @@ public class BallsCommand : ModuleBase
         await ReplyAsync(
             "Zarachy Salore's balls are twice the size of Ray's. All hail the wielder of massive balls!");
     }
+    
+    [Command("fishfucker")]
+    [RequireRole(853369121000521780)]
+    public async Task Fishfucker()
+    {
+        await ReplyAsync(
+            "Zarachy Salore is a fishfucker: https://docs.google.com/document/d/1-RSf8WQxxJpmix6DX2oVIA0wN_UN75NY9K_GsdC4Dkg");
+    }
 }
